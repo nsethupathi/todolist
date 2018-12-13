f63ef82aba909e2daa0634fe8aa1da5dc9806d93
Nivethan <nivethan.sethupathi@gmail.com>
2018-12-12 18:05:42 -0600

    adding DataBase connection class

---
+++ todolist/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace todolist
+{
+    class DatabaseConnection
+    {
+        MySqlConnection connectionstring = new MySqlConnection("server=localhost;user id=root;database=independent");
+
+        public void OpenConnection()
+        {
+            connectionstring.Open();
+        }
+
+        public void CloseConnection()
+        {
+            connectionstring.Close();
+        }
+    }
+}
